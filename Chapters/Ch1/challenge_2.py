a68ef75e5f29b84734bbdf20d53e4bcfe0772cb2
Farah ÇELEBİ <farah.h.celebi@gmail.com>
2024-07-28 15:09:07 +0300

    Challenge 2

---
+++ Chapters/Ch1/challenge_2.py
@@ -0,0 +1,11 @@
+# 1. Ask user to enter a season
+season = input('Enter a season: ')
+
+# 2. Ask user to enter a number
+number = input('Enter a season: ')
+
+# 3. Ask user to enter an adjective
+adjective = input('Enter an adjective: ')
+
+# 4. Print the following sentence
+print(f'On a {adjective} {season} day, I drink at least {number} cups of juice! ')
